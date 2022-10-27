569aa1a0db0270a79d7831aa09151dedae353706
Manuela Lemos <a47235@alunos.isel.pt>
2022-10-27 10:29:17 +0100

    dec done

--- tp1/java/src/main/java/_6/dec.java
+++ tp1/java/src/main/java/_6/dec.java
@@ -1,7 +1,11 @@
 package _6;
 
+import utils.FileOperations;
+
 import javax.crypto.Cipher;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import java.security.Key;
 import java.security.KeyStore;
 import java.security.NoSuchAlgorithmException;
@@ -10,27 +14,33 @@ public class dec {
 /*
     filename dec.decode(FilenameCifer, FilenameKey, keystore);
 */
-    public String decode(String FilenameCifered, String FilenameKey, KeyStore keystore) throws Exception {
-        Cipher cifra;
+    public String decode(String filenameCiphered, String filenameKey, KeyStore keystore) throws Exception {
+
         try{
-            // TODO add clear cipher
-            cifra = Cipher.getInstance("");
+            Cipher cifraSim = Cipher.getInstance("AES/GCM/NOPADDING");
+            Cipher cifraAssim = Cipher.getInstance("RSA");
+            Key privKey = keystore.getKey(keystore.aliases().nextElement(), "changeit".toCharArray());
+
+            // Get simetric key
+            byte[] cipheredKey = FileOperations.readFile64(filenameKey);
+            cifraAssim.init(Cipher.DECRYPT_MODE, privKey);
+            byte[] decipheredKey = cifraAssim.doFinal(cipheredKey);
+
+            // Decode file
+            byte[] cipheredFile = FileOperations.readFile64(filenameCiphered);
+            SecretKey secretKey = new SecretKeySpec(decipheredKey, "AES");
+            cifraSim.init(Cipher.DECRYPT_MODE, secretKey);
+            byte [] decipheredFile = cifraSim.doFinal(cipheredFile);
+
+            // Write to output
+            String outFile = "deciphered_" + filenameCiphered;
+
+            FileOperations.writeToFile(decipheredFile,outFile);
+            return outFile;
         } catch (NoSuchPaddingException | NoSuchAlgorithmException e) {
             // should not happen
             throw new Exception("The cipher does not exist");
         }
-        Key privKey = keystore.getKey(keystore.aliases().nextElement(), "changeit".toCharArray());
-        cifra.init(Cipher.DECRYPT_MODE, privKey);
-
-        // TODO
-        byte [] fileBytes = new byte[1];
-        //TODO change to get file in Base64
-
-
-        cifra.update(fileBytes);
-
-        /*TODO: change*/
-        return null;
     }
 
 }
